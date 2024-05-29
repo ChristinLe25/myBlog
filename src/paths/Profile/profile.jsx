c1f372c916997b640bc0281d10b40d855f9c4a8d
Christin <christin25@hotmail.se>
2024-05-29 18:35:05 +0200

    klar innan finlir

--- src/paths/Profile/profile.jsx
+++ src/paths/Profile/profile.jsx
@@ -1,9 +1,18 @@
+import { useAuthState } from "react-firebase-hooks/auth";
 import Header from "../../Header/Header";
+import { auth } from "../../config/firebase";
 
 
 
 const Profile = () => { 
-    
+  const [user] = useAuthState(auth);
+  if(user == null || user == undefined){
+    return(
+      <>
+      <p>Can't acces page</p>
+      </>
+    )
+  }
     return (
       <div>
 
